eb647e64a556be461e367a0e41dbd70ed3f168db
TrueXz <source_2002@mail.ru>
2021-11-06 19:49:10 +0500

    Timer complete

---
+++ app/src/main/java/com/example/homegym/Menu.java
@@ -0,0 +1,70 @@
+package com.example.homegym;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Menu extends AppCompatActivity {
+Button start, stop, reset;
+TextView time;
+int mCurrentPeriod = 0;
+private Timer myTimer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        start = findViewById(R.id.Start);
+        stop = findViewById(R.id.Stop);
+        reset = findViewById(R.id.Reset);
+        time = findViewById(R.id.Time);
+    }
+
+    public void Start(View view){
+        start.setEnabled(false);
+        myTimer = new Timer();
+        myTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                TimerMethod();
+            };
+        },0 ,1000);
+    }
+
+    public void Stop(View view){
+        if(myTimer != null)
+            myTimer.cancel();
+        start.setEnabled(true);
+    }
+
+    public void Reset(View view){
+        if(myTimer != null){
+            mCurrentPeriod = 0;
+            myTimer.cancel();
+            time.setText("00:00");
+            start.setEnabled(true);
+        }
+    }
+
+    private void TimerMethod(){
+        this.runOnUiThread(Timer_tick);
+    }
+
+    private Runnable Timer_tick = new Runnable() {
+        @Override
+        public void run() {
+            mCurrentPeriod++;
+            String temp = (new SimpleDateFormat("mm:ss")).format(new Date(mCurrentPeriod*1000));
+            time.setText(temp);
+        }
+    };
+}
--- app/src/main/java/com/example/homegym/SignIn.java
+++ app/src/main/java/com/example/homegym/SignIn.java
@@ -6,7 +6,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -40,7 +39,7 @@ DBHelper DB;
         signIn.setOnClickListener(v -> {
             if (check_result() == true) {
                 Toast.makeText(getApplicationContext(), "Успешный вход", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(SignIn.this, Activity.class);
+                Intent intent = new Intent(SignIn.this, Menu.class);
                 startActivity(intent);
                 finish();
             }
